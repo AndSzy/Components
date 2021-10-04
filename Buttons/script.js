72a805f7e2bd8ebffce1d4accf6ad869a949cafd
AndSzy <aszycher@gmail.com>
2021-10-04 20:12:33 +0100

    Buttons files cleareda and debugged.

--- Buttons/script.js
+++ Buttons/script.js
@@ -1,6 +1,8 @@
+// 1. Dropdown with inputs
+
 // keep an .active class when inbox checked - css style
 
-const inputElements = document.querySelectorAll('input[type=checkbox]');
+const inputElements = document.querySelectorAll('#dropdown-inputs input[type=checkbox]');
 
 inputElements.forEach((input) => {
     input.addEventListener('change', () => {
